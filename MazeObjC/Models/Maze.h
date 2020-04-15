48fecbf35d7ba8202e2200d41b1e2876d0ed7b7f
Huy Vo <voxqhuy@gmail.com>
2020-04-15 12:19:45 -0400

    add maze header

---
+++ MazeObjC/Models/Maze.h
@@ -0,0 +1,19 @@
+//
+//  Maze.h
+//  MazeObjC
+//
+//  Created by Huy Vo on 4/15/20.
+//  Copyright © 2020 Huy Vo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef Maze_h
+#define Maze_h
+
+
+@interface Maze : NSObject
+
+@end
+
+#endif /* Maze_h */
